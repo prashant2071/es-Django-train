265d2489e533836630facec2b5739c821aebeb85
prashanta <pbt9840@gmail.com>
2023-11-28 09:17:54 +0545

    fix:error in foodmanduclass.py

--- pratice/foodmanduclass.py
+++ pratice/foodmanduclass.py
@@ -18,11 +18,13 @@ class FoodmanduFetch:
              "price":menu["price"]
          }
          output.append(tmp)
-        headers = output[0].keys()   
-        with open("foodmandu-menu.csv","w") as file_object:
-            dict_writer = csv.DictWriter(file_object, headers)
-		    dict_writer.writeheader()
-		    dict_writer.writerows(output) 
+        headers = output[0].keys()  
+         
+        with open('foodmandu-menus.csv', 'w') as file_object:
+        
+          dict_writer.writerows(output) 
+          dict_writer = csv.DictWriter(file_object, headers)
+          dict_writer.writeheader()
 
 s= FoodmanduFetch()
 s.scrape_menu(1027)
